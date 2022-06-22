06d06bc3f01f28c33ce052208d772456fa4cb1fc
DELL <DELL@DESKTOP-6UO4B1H>
2022-06-22 09:11:26 -0700

    Add project files.

---
+++ TraningS/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TraningS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
